043f023650f851e58d6db60ccfe18f359634e0a9
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2025-07-10 13:43:17 +0200

    interaction with squares

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const Square = ({ value }) => {
+  function handleClick() {
+    console.log("Square clicked with value:", value);
+    alert(`Square clicked! Value: ${value}`);
+  }
+
   return (
-    <button className="square">{value}</button>
+    <button
+      className="square"
+      onClick={handleClick}  
+    >
+      {value}
+    </button>
   )
 }
 
